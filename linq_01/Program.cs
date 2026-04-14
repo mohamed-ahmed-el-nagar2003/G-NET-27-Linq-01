adff3460dbe229c5bad1e173ef781773496f396b
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-14 14:21:45 +0200

    Q_02

--- linq_01/Program.cs
+++ linq_01/Program.cs
@@ -9,9 +9,14 @@ namespace linq_01
         {
             #region Q_01
             //1. Get all products from the "Seafood" category. Print eachproduct's name and price.
-           // var result = Source.ProductList.Where(p => p.Category == "Seafood").Select(p => $"Product Name :{p.ProductName}  Price :{p.UnitPrice}");
+            // var result = Source.ProductList.Where(p => p.Category == "Seafood").Select(p => $"Product Name :{p.ProductName}  Price :{p.UnitPrice}");
             #endregion
 
+            #region Q_02
+            //Get a list of only the product names from ProductList. Printeach name.
+           // var result = Source.ProductList.Select(p => p.ProductName  );
+
+            #endregion
 
             //foreach (var item in result)
             //{
