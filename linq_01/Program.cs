ae2ae0ab02c4849e26dd1c8147b0866021a17809
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-14 14:13:22 +0200

    Q_01

--- linq_01/Program.cs
+++ linq_01/Program.cs
@@ -7,7 +7,16 @@ namespace linq_01
     {
         static void Main(string[] args)
         {
-           
+            #region Q_01
+            //1. Get all products from the "Seafood" category. Print eachproduct's name and price.
+           // var result = Source.ProductList.Where(p => p.Category == "Seafood").Select(p => $"Product Name :{p.ProductName}  Price :{p.UnitPrice}");
+            #endregion
+
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
