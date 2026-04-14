ff45f2802830106607267fd4d7f1ca8f1ee8a2a3
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-14 14:30:03 +0200

    Q_03

--- linq_01/Program.cs
+++ linq_01/Program.cs
@@ -14,8 +14,13 @@ namespace linq_01
 
             #region Q_02
             //Get a list of only the product names from ProductList. Printeach name.
-           // var result = Source.ProductList.Select(p => p.ProductName  );
+            // var result = Source.ProductList.Select(p => p.ProductName  );
 
+            #endregion
+
+            #region Q_03
+            //Sort all products by UnitPrice (ascending). Print each product's name and price.
+            //var result = Source.ProductList.OrderBy(p => p.UnitPrice).Select(p => new {p.ProductName , p.UnitPrice});
             #endregion
 
             //foreach (var item in result)
