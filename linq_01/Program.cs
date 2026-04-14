7cd6175da2ac977cc6f3df1fa6421ec9ae11d6ff
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-04-14 14:38:59 +0200

    Q_05

--- linq_01/Program.cs
+++ linq_01/Program.cs
@@ -25,8 +25,13 @@ namespace linq_01
 
             #region Q_04
             //4. Get all products where UnitPrice is between 10 and 30
-           // var result = Source.ProductList.Where(p => p.UnitPrice > 10 && p.UnitPrice < 30);
+            // var result = Source.ProductList.Where(p => p.UnitPrice > 10 && p.UnitPrice < 30);
 
+            #endregion
+
+            #region Q_05
+            //5. Get all products that are in stock (UnitsInStock > 0) and belong to the "Condiments" category.
+           // var result = Source.ProductList.Where(p => p.UnitsInStock > 0 && p.Category == "Condiments");
             #endregion
 
             //foreach (var item in result)
